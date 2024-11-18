b0f2ee154059faaa2239c53cb196ddee3c3672a6
Andrei Lupsa <andrei.lupsa@endava.com>
2024-11-18 17:09:04 +0200

    Added an implementation

--- src/test/java/com/polarbookshop/quotefunction/QuoteFunctionApplicationTests.java
+++ src/test/java/com/polarbookshop/quotefunction/QuoteFunctionApplicationTests.java
@@ -1,13 +1,60 @@
 package com.polarbookshop.quotefunction;
 
+import com.polarbookshop.quotefunction.domain.Genre;
+import com.polarbookshop.quotefunction.domain.Quote;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QuoteFunctionApplicationTests {
 
+    @Autowired
+    WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void whenAllQuotesThenReturn() {
+        webTestClient
+                .get()
+                .uri("/")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Quote.class);
+    }
+
+    @Test
+    void whenRandomQuoteThenReturn() {
+        webTestClient
+                .get()
+                .uri("/randomQuote")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Quote.class);
+    }
+
+    @Test
+    void whenGenreQuoteThenReturn() {
+        webTestClient
+                .post()
+                .uri("/genreQuote")
+                .bodyValue("FANTASY")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Quote.class)
+                .value(quote -> assertThat(quote.genre()).isEqualTo(Genre.FANTASY));
+    }
+
+    @Test
+    void whenGenreQuoteLogQuoteThenReturn() {
+        webTestClient
+                .post()
+                .uri("/genreQuote,logQuote")
+                .bodyValue("FANTASY")
+                .exchange()
+                .expectStatus().is2xxSuccessful();
     }
 
 }
